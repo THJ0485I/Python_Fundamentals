0ff13beb9ab4a434c7d954a15f3f5fa397dcc74e
HongJiang47_YT <94294806+THJ0485I@users.noreply.github.com>
2024-05-14 17:57:28 +0800

    Add files via upload

---
+++ Parameterised Functions and Argument Passing.py
@@ -0,0 +1,14 @@
+"""
+Lesson Outline: Arguments passing in Python functions
+
+5 types of arguments:
+
+1. default arguments
+
+2. fixed keyword arguments
+3. fixed positional arguments
+
+4. arbitrary keyword arguments
+5. arbitrary positional arguments
+"""
+
